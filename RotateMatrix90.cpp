cb99026b3740edabe9a41d28051adcd1788b14cb
Mariglen Jahaj <31398575+mJahaj@users.noreply.github.com>
2018-02-22 21:30:21 -0500

    Add files via upload

---
+++ RotateMatrix90.cpp
@@ -0,0 +1,45 @@
+/*
+	Mariglen Jahaj
+	1/12/2017
+	This class rotates a matrix 90 degrees clockwise and prints out matrix
+*/
+#include "RotateMatrix90.h"
+#include <vector>
+#include <iomanip>
+#include <iostream>
+RotateMatrix90::RotateMatrix90()
+{
+
+}
+
+RotateMatrix90::RotateMatrix90(std::vector<std::vector<int> > matrix)
+{
+    this->matrix = matrix;
+    //this->rows = rows;
+    //this->cols = cols;
+}
+
+std::vector<std::vector <int> > RotateMatrix90::RotMatrix90(){
+  
+  std::vector <std::vector <int> > matrix90;
+
+  for(int i = 0; i < matrix.size(); i++){
+    matrix90.push_back(std::vector <int>());
+
+    for(int j = matrix[0].size()-1; j >= 0; j--){
+      matrix90[i].push_back(matrix[j][i]);
+    }
+  }
+    return matrix90;
+}
+
+void RotateMatrix90::printMatrix(std::vector<std::vector<int> > matrix)
+{
+  for(int i = 0; i < matrix.size(); i++){
+
+		for(int j = 0; j < matrix[0].size(); j++){
+		  std::cout<<std::setw(2) << matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
---
+++ RotateMatrix90.h
@@ -0,0 +1,29 @@
+/*
+	Mariglen Jahaj
+	1/12/2017
+	This class rotates a matrix 90 degrees clockwise and prints out matrix
+*/
+#ifndef ROTATEMATRIX90_H
+#define ROTATEMATRIX90_H
+#include <vector>
+#include <iomanip>
+#include <iostream>
+class RotateMatrix90
+{
+    public:
+        RotateMatrix90();
+        RotateMatrix90(std::vector<std::vector <int> > matrix);
+        std::vector<std::vector <int> > RotMatrix90();//returns a matrix rotated 90 degrees clockwise
+        void printMatrix(std::vector<std::vector<int> > matrix);
+        void setMatrix(std::vector<std::vector<int> > matrix)
+        {this->matrix = matrix;}
+        //void setRows(int rows);
+        //void setCols(int cols);
+
+    private:
+        std::vector<std::vector<int> > matrix;
+        //int rows;
+        //int cols;
+};
+
+#endif // ROTATEMATRIX90_H
